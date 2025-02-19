60c3b5f87245e0f6a2dcafaa329a7792942e7e1e
Vũ Minh Đức <mduc152001@gmail.com>
2025-02-19 22:19:39 +0700

    init vite-ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Button type="primary">Test button</Button>
+    </div>
+  );
+}
+
+export default App;
